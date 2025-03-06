27906fb5b6ebe6ee9f9a9abc8841b315aa366621
MichelPianaro <teconhecer27@gmail.com>
2025-03-06 11:11:26 -0300

    código para testar a inserção de dois comentários

--- src/components/PostComments/PostComments.test.tsx
+++ src/components/PostComments/PostComments.test.tsx
@@ -1,5 +1,4 @@
 import { fireEvent, render, screen } from '@testing-library/react';
-import Post from '.';
 import PostComment from '.';
 
 describe('Teste para o componente PostComment', () => {
@@ -7,4 +6,25 @@ describe('Teste para o componente PostComment', () => {
         render(<PostComment/>);
         expect(screen.getByText('Comentar')).toBeInTheDocument();
     });
-});
+
+    it('Deve adicionar 2 comentários no Post', () => {
+        render(<PostComment />);
+        fireEvent.change(screen.getByTestId('campo-comentario'), {
+            target: {
+                value: 'Uaaal incrivél!!!'
+            }
+        })
+        fireEvent.click(screen.getByTestId('btn-comentar'))
+
+        fireEvent.change(screen.getByTestId('campo-comentario'), {
+            target: {
+                value: 'Que Show, quero um desses!'
+            }
+        })
+        fireEvent.click(screen.getByTestId('btn-comentar'))
+
+
+        expect(screen.getByText('Uaaal incrivél!!!')).toBeInTheDocument();
+        expect(screen.getByText('Que Show, quero um desses!')).toBeInTheDocument();
+    })
+})
